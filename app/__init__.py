ef301669bbdba6d8f0b73a04a9d5fdb8b6c92a16
teodorklaus <sergey.gorshok@gmail.com>
2017-09-29 23:38:32 +0300

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+from config import app_config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to acceess this page."
+    login_manager.login_view = "auth.login"
+
+    return app
